c751e610cbf341c2f1a16d24b664ef1a05e19b51
uladzislauza <uladzislauza@playtika.com>
2023-07-02 18:51:56 +0300

    add GA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Header } from '@/components/Header'
 import { MainContent } from '@/components/MainContent'
 import Head from 'next/head'
 import favicon from  "../../public/favicons/favicon.png"
+import Script from 'next/script'
 
 export default function Home() {
   return (<>
@@ -16,21 +17,30 @@ export default function Home() {
         Digital assets, Portfolio management, Payment processing, Decentralization, Peer-to-peer, Smart contracts, Tokenization, Mining
         Crypto regulation, User experience, Mobile app, Web development, Customer support, KYC/AML compliance, Cryptocurrency news, Cryptocurrency market trends"
       />
-   
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
         href={favicon.src}
       />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href={favicon.src}
-    />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={favicon.src}
+      />
       <title>LetsBulls</title>
       </Head>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z6VPSNKS9T"></Script>
+      <Script id="google-analytics">
+        {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+         
+           gtag('config', 'G-Z6VPSNKS9T');
+        `}
+      </Script>
       <Header />
       <MainContent />
       <Footer />
